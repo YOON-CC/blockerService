0382968f4af07176025ac4967fbb4743b9d06af8
YOON-CC <whdbscks77@gmail.com>
2024-01-29 12:00:00 +0900

    feat(ContractWrite.tsx): 계약서 작성 api 호출

--- src/app/(afterLogin)/write/_component/ContractWrite.tsx
+++ src/app/(afterLogin)/write/_component/ContractWrite.tsx
@@ -3,14 +3,62 @@
 import React, { useState, useEffect} from 'react';
 import axios from 'axios';
 // import { uploadImages, postBoard } from '@/api/postWrite';
-import styles from "@/app/(afterLogin)/postwrite/_component/postwrite.module.css"
+import styles from "@/app/(afterLogin)/write/_component/contractWrite.module.css"
+import Link from 'next/link';
 
 
 const ContractWrite = () => {
 
+    const access_token = typeof window !== 'undefined' ? localStorage.getItem('access-token') : null;
+
+
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    const handletitleChange = (event : any) => {
+        setTitle(event.target.value)
+    };
+    const handlecontentChange = (event : any) => {
+        setContent(event.target.value)
+    };
+
+    const handleContractPost = async (event: any) => {
+        event.preventDefault();
+        try {
+            const response = await axios.post(`${process.env.NEXT_PUBLIC_REACT_APP_API_URL}/contracts`, {
+                title: title,
+                content: content,
+            }, {
+                headers: {
+                    'Authorization': access_token,
+                }
+            });
+    
+            if (response.status === 201) {
+                console.log("옴");
+                return true;
+            }
+    
+            return false;
+        } catch (error) {
+            // 에러 처리
+            throw error;
+        }
+    };
 
     return (
-        <>계약서 작성 페이지</>
+        <div className={styles.Container}>
+            <input className={styles.Container_title} placeholder='제목을 작성해주세요.' onChange={handletitleChange}></input>
+            <textarea className={styles.Container_content} placeholder='내용을 작성해주세요.' onChange={handlecontentChange}></textarea>
+            <div className={styles.Container_btn_container}>
+                <Link href="/contracts" style={{ textDecoration: 'none' }}>
+                    <div className={styles.Container_btn_container_b1}>취소</div>
+                </Link>
+                <form onSubmit={handleContractPost}>
+                    <button className={styles.Container_btn_container_b2}>작성</button>
+                </form>
+            </div>
+        </div>
     );
 };
 
