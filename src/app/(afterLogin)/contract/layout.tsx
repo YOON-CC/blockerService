21c1b0708ec250daeffb44cec543e4614a130d50
YOON-CC <whdbscks77@gmail.com>
2024-01-31 13:41:49 +0900

    feat(contract/대시보드 레이아웃 완성)

--- src/app/(afterLogin)/contract/layout.tsx
+++ src/app/(afterLogin)/contract/layout.tsx
@@ -4,7 +4,22 @@ import styles from '@/app/(afterLogin)/contract/layout.module.css'
 type Props = { children: ReactNode };
 export default function Layout({ children }: Props) {
   return (
-    <div className={styles.container}>
+    <div className={styles.contractContainer}>
+      <div className={styles.contractContainer1}>
+        <div className={styles.contractContainer1Layout1}>
+          <div className={styles.contractContainer1Layout1Title}>Progress</div>
+        </div>
+        <div className={styles.contractContainer1Layout2}></div>
+      </div>
+      <div className={styles.contractContainer2}>
+        <div className={styles.contractContainer2Layout1}></div>
+        <div className={styles.contractContainer2Layout2}></div>
+        <div className={styles.contractContainer2Layout3}></div>
+      </div>
+      <div className={styles.contractContainer3}>
+        <div className={styles.contractContainer3Layout1}></div>
+        <div className={styles.contractContainer3Layout2}></div>
+      </div>
       {children}
     </div>
   )
